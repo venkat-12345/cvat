2ae3e03a7f5805f9b46382f1f5adbd921d3b1e23
murthylogeshwar <33926871+murthylogeshwar@users.noreply.github.com>
2020-02-13 01:55:28 +0530

    Revised changes

--- master_class_kitti.py
+++ master_class_kitti.py
@@ -1,9 +1,12 @@
+# class to convert cvat-xml file containing annotations into KITTI format
+
+
 # import statements
 
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-class xml_to_kitti:
+class cvat_xml_to_kitti:
 
   """ 
   Class for converting data into KITTI format from annotated cvat-xml format.
@@ -17,16 +20,40 @@ class xml_to_kitti:
       Character to separate attributes and label name in KITTI file.
 
   """
+
   def __init__(self,path_to_xml_file,attribute_separator='*'):
     
-    # converting the file path into a pathlib path variable and storinng the folder location
+    """
+    Converting the file path into a pathlib path variable and storing the folder location.
+
+    """
+
     path1=Path(path_to_xml_file)
     self.path_to_folder=Path(path1.parents[0])
             
     # Generating the soup variable 
     self.soup = BeautifulSoup(open(path_to_xml_file), 'xml')
 
+    # Assigning the separating charachter between two entries
     self.attribute_separator=attribute_separator
+
+
+  def create_folder(self,folder_name):
+
+    """
+    Function to create a folder in the same directory as the cvat-xml file and returns the path of the created folder.
+
+    Parameters
+    -----------
+
+    folder_name:
+      
+      Name of the folder to be created.
+    
+    """
+
+    Path(self.path_to_folder,folder_name).mkdir(exist_ok = True)
+    return Path(self.path_to_folder,folder_name)
 
 
   def write_specific_labels_with_attributes(self,specific_label_list=dict()):
@@ -45,55 +72,55 @@ class xml_to_kitti:
 
     """   
     
-    # case : incorrect datatype passed
+    # case : incorrect datatype passed ; Raises an error
     
     if(type(specific_label_list)!=dict):
-        raise Exception('Incorrect Datatype : Specific_label_list is not a dictionary')
-        return
-    
+
+      raise Exception('Incorrect Datatype : Specific_label_list is not a dictionary')
+      return
+
+    # Case : Empty dictionary is passed ; Writes labels with all attributes.
+
+    if(len(specific_label_list)==0):
+
+      self.write_kitti_with_attributes()
+      return
+
 
     # creating a folder for the files
 
-    path_to_specific_label_folder=Path(self.path_to_folder,'specific_labels')
-    path_to_specific_label_folder.mkdir(exist_ok = True)
+    path_to_specific_labels_folder=self.create_folder('specific_labels')
 
-    
-    # Case : Empty dictionary is passed ; Writes labels with all attributes
-    if(len(specific_label_list)==0):
+    # Finding all tags named as 'box'
+    for i in self.soup.find_all('box'):
 
-      raise Exception("Required labels not specified : Empty dictionary passed")
-      return
+        # Reading the image name from the file and creating all the parent directories in the image name and creating a path with the image name with a .txt extension for writing the kitti files 
 
-    # Case : Mentioned labels and attributes are picked and written on to the file
-    else:
-        
-        # Finding all tags named as 'box'
-        for i in self.soup.find_all('box'):
+        img_path=Path(path_to_specific_labels_folder,i.parent.attrs['name'])
+        path_to_specific_label_file=img_path.with_suffix(".txt")
+        path_to_specific_label_file.parent.mkdir(parents=True, exist_ok=True)
 
 
-            img_path=Path(i.parent.attrs['name'])
-            image_name=img_path.stem
-            path_to_specific_label_file=Path(path_to_specific_label_folder, image_name+'.txt')
+        # Mentioned labels and attributes are picked and written on to the file
 
-            f1= open(path_to_specific_label_file,"a")
-        
-            modified_label=list()
-        
-            if i.attrs['label'] in specific_label_list.keys():
-        
-                for j in i.find_all('attribute'):
-                
-                    for k in specific_label_list[i.attrs['label']]:
-                
-                        if j.attrs['name'] == k:
-                    
-                            modified_label.append(j.text)
-        
-                modified_label.append(i.attrs['label'])
+        modified_label=list()
+
+        if i.attrs['label'] in specific_label_list.keys():
+                  
+            for j in i.find_all('attribute'):
+
+                for k in specific_label_list[i.attrs['label']]:
+
+                    if j.attrs['name'] == k:
+
+                        modified_label.append(j.text)
+
+            modified_label.append(i.attrs['label'])
+                  
+            with open(path_to_specific_label_file,"a") as f1:
+
+              f1.write(f"{self.attribute_separator.join(modified_label)} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
             
-                f1.write(f"{self.attribute_separator.join(modified_label)} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
-            
-            f1.close()
 
   def write_kitti_with_attributes(self):
     
@@ -105,8 +132,7 @@ class xml_to_kitti:
     # Initialising a empty list to collect all distinct attributes
     distinct_labels=list()
 
-    path_to_kitti_with_attributes_folder=Path(self.path_to_folder,'kitti_with_attributes')
-    path_to_kitti_with_attributes_folder.mkdir(exist_ok = True)
+    path_to_kitti_with_attributes_folder=self.create_folder('kitti_with_attributes')
 
 
     for i in self.soup.find_all('box'):
@@ -119,23 +145,26 @@ class xml_to_kitti:
                 
           distinct_labels.append(y.attrs['name'])
 
+    # Writing a template_file with the header label names
 
     path_to_template_file=Path(path_to_kitti_with_attributes_folder,'template_file.txt')
-    f2=open(path_to_template_file,'a')
-    f2.write("Kitti files have the following attribute's text appended with their label\n")
-    f2.write(f"{distinct_labels}\n\n")
+
+    with open(path_to_template_file,'a') as f2:
+
+      f2.write("Kitti files have the following attribute's text appended with their label\n")
+      f2.write(f"{distinct_labels}\n\n")
 
     for i in self.soup.find_all('box'):
     
       attribute_label_dict=dict()
       modified_label=list()
 
-      img_path=Path(i.parent.attrs['name'])
-      image_name=img_path.stem
-      path_to_kitti_with_attributes_file=Path(path_to_kitti_with_attributes_folder, image_name+'.txt')
+      # Reading the image name from the file and creating all the parent directories in the image name and creating a path with the image name with a .txt extension for writing the kitti files 
 
-      f1= open(path_to_kitti_with_attributes_file,"a")
-    
+      img_path=Path(path_to_kitti_with_attributes_folder,i.parent.attrs['name'])
+      path_to_kitti_with_attributes_file=img_path.with_suffix(".txt")
+      path_to_kitti_with_attributes_file.parent.mkdir(parents=True, exist_ok=True)
+
       for j in i.find_all('attribute'):
             
         # Dictionary storing attribute along with their text
@@ -153,52 +182,30 @@ class xml_to_kitti:
           modified_label.append(attribute_label_dict[y])
     
       modified_label.append(i.attrs['label'])
-            
-      f1.write(f"{self.attribute_separator.join(modified_label)} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
 
-      f1.close()
+      with open(path_to_kitti_with_attributes_file,'a') as f1:
+            
+        f1.write(f"{self.attribute_separator.join(modified_label)} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
+
 
     # just read the xml and create normal kitti file
 
   def write_kitti_without_attributes(self):
-
 
     """
     Reads a xml file and creates a folder containing text files (<image_name>.txt) containing annotated data in kitti format.
 
     """
 
-    path_to_kitti_files_folder=Path(self.path_to_folder,'kitti_files')
-    path_to_kitti_files_folder.mkdir(exist_ok = True)
+    path_to_kitti_files_folder=self.create_folder('kitti_files')
 
     for i in self.soup.find_all('box'):
 
-      img_path=Path(i.parent.attrs['name'])
-      image_name=img_path.stem
-      path_to_kitti_file=Path(path_to_kitti_files_folder, image_name+'.txt')
+      # Reading the image name from the file and creating all the parent directories in the image name and creating a path with the image name with a .txt extension for writing the kitti files 
+      img_path=Path(path_to_kitti_files_folder,i.parent.attrs['name'])
+      path_to_kitti_file=img_path.with_suffix(".txt")
+      path_to_kitti_file.parent.mkdir(parents=True, exist_ok=True)
 
-      f1=open(path_to_kitti_file,'a')
+      with open(path_to_kitti_file,'a') as f1:
 
-      f1.write(f"{i.attrs['label']} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
-
-    f1.close()
-
-
-
-# Testing //**//**//**//**/*/*/*/*/*/*/*
-
-path_to_xml='/home/murthylogeshwar/Desktop/padipu/detect/test/PPE - BPCL Mumbai Jul 2019 TPulse vids - Day 7-12.xml'
-
-k=xml_to_kitti(path_to_xml)
-k.write_kitti_with_attributes()
-k.write_kitti_without_attributes()
-
-specific_label_list={'Helmet':['colour'],'Suit':['reflective'],'Missing_PPE':['Sleeping_Person','type']}
-k.write_specific_labels_with_attributes(specific_label_list)
-
-
-#specific_label_list=dict()
-#k.write_specific_labels_with_attributes(specific_label_list)
-
-#specific_label_list=4
-#k.write_specific_labels_with_attributes(specific_label_list)
+        f1.write(f"{i.attrs['label']} 0.00 {i.attrs['occluded']} 0.00 {i.attrs['xtl']} {i.attrs['ytl']} {i.attrs['xbr']} {i.attrs['ybr']} 0.00 0.00 0.00 0.00 0.00 0.00 0.00\n")
